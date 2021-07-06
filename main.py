05a0c9b31dec9efb40b4c79f1159673ed8d13809
keystroke3 <37796862+keystroke3@users.noreply.github.com>
2021-07-06 15:05:11 +0300

    refactored main.py to make it DRY

--- main.py
+++ main.py
@@ -1,42 +1,53 @@
 from flask import Flask, request, redirect
 import streamlink
 import validators
-import sys
 
 app = Flask(__name__)
+
+
+def get_stream_obj(query):
+    try:
+        links = streamlink.streams(query)
+        qualities = list(links.keys())
+        return (links, qualities)
+    except Exception:
+        return f"Could not get the link, Streamlink couldn't read {query}"
+
 
 @app.route("/")
 def index():
     return "This program permits you to get direct access to streams by using Streamlink. Alternative link : https://iptv--iptv.repl.co/streamlink?url=*insert url*. Enjoy ! LaneSh4d0w."
 
-@app.route('/iptv-query')
-def home():
-  if request.args:
-    if request.args.get('streaming-ip') == "" :
-      return "No streaming IP found : reason = query string is empty"
-    else:
-      valid=validators.url(request.args.get('streaming-ip'));
-      if valid == True:
-        if "quality" in request.args:
-          if request.args.get("quality") == "unsure":
-            try:
-              stream_qualities = streamlink.streams(request.args.get('streaming-ip'))
-              sorted_list = list(stream_qualities.keys())
-              string_of_the_list = ', '.join(sorted_list)
-              return ("Available qualities = " + string_of_the_list)
-            except:
-              return ("Could not get the link, Streamlink couldn't read ", request.args.get('streaming-ip'))
-          else:    
-            return redirect(streamlink.streams(request.args.get('streaming-ip'))[request.args.get('quality')].url)
-        else:
-          return redirect(streamlink.streams(request.args.get('streaming-ip'))['best'].url)
-      else:
-        return "The URL you've entered is not valid."
-  else:
-    return "You provided nothing to me."
 
-if __name__ == '__main__':
-  app.run(
-    host='0.0.0.0',
-    port=8888
-  )
+@app.route("/iptv-query")
+def home():
+    if request.args:
+        query = request.args.get("streaming-ip")
+        if not query:
+            return "No streaming IP found. Reason: streaming-ip string is empty"
+
+        valid = validators.url(query)
+        if not valid:
+            return "The URL you've entered is not valid."
+
+        # get stream data or exit with error message
+        stream_obj = get_stream_obj(query)
+        if type(stream_obj) == str:
+            return stream_obj
+
+        # Use specified quality or "best"
+        quality = request.args.get("quality")
+        if not quality:
+            return redirect(stream_obj[0]["best"].url)
+        elif quality in stream_obj[1]:
+            return redirect(stream_obj[0][quality].url)
+        else:
+            # if quality invalid, list available qualities
+            qualities = ",".join(stream_obj[1])
+            return f"Available qualities are: {qualities}"
+    else:
+        return "You provided nothing to me."
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8888)
