2ffd05f4acd5373fc4aa130a92a7677b59d657b8
keystroke3 <37796862+keystroke3@users.noreply.github.com>
2021-07-10 19:28:50 +0300

    added streamlink error exceptions

--- api/api.py
+++ api/api.py
@@ -1,11 +1,19 @@
 import streamlink
+from streamlink import (
+    StreamError,
+    StreamlinkError,
+    PluginError,
+    NoPluginError,
+    NoStreamsError,
+)
 
 
 class Fetch:
     """
-        Gets data from host, filters it and returns streams
-        (query: str, quality: str,list,tuple)
+    Gets data from host, filters it and returns streams
+    (query: str, quality: str,list,tuple)
     """
+
     def __init__(self, query, quality):
         self.query = query
         if not quality:
@@ -17,8 +25,8 @@ class Fetch:
 
     def get_streams(self):
         """
-            Get data streams and resolutions
-            Returns: (links, resolution), Error string
+        Get data streams and resolutions
+        Returns: (links, resolution), Error string
         """
         try:
             # FIXME has issues if a channel is hosting another on Twitch
@@ -26,21 +34,27 @@ class Fetch:
             res = list(links.keys())
             return (links, res)
         except Exception:
-            return 1
+            # returnes exceptions raised by streamlink
+            raise
 
     def filtered_streams(self):
         """
-            Filter streams according to specified quality.
-            Default quality: best
-            Returns: {quality: stream_url}
+        Filter streams according to specified quality.
+        Default quality: best
+        Returns: {quality: stream_url}
         """
         try:
-            streams, resolutions = self.get_streams()
+            payload = self.get_streams()
+            streams, resolutions = payload
             if not streams:
                 raise ValueError
             res_str = ",".join(resolutions)
+        except PluginError as pe:
+            return str(pe)
         except ValueError:
             return f"Could not get the link, Streamlink couldn't read {self.query}"
+        except TypeError:
+            return payload
 
         # TODO: find a more elegant solution to deal with 'best' and 'worst'
         if "best" in self.qualities:
