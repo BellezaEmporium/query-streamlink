30d84ffd4f8921fcf187d4c1b7feee156f5d67ac
Omega <lanesh4d0w@gmail.com>
2022-04-23 19:12:40 +0200

    clean up

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 import streamlink
 from streamlink import NoPluginError, PluginError
 
+
 def get_streams(query):
     """
     Get data streams
@@ -14,13 +15,13 @@ def get_streams(query):
             for quality, link in streams:
                 # Suggest that if there's no multiple qualities (live),
                 # give manifest (master) URL.
-                if "live" not in quality:
+                if "live" not in quality or "best" in quality:
                     return link.to_manifest_url()
                 else:
                     return link.to_url()
     except ValueError as ex:
         return f"Streamlink couldn't read {query}, for this reason : {ex}"
     except NoPluginError:
-        return f"Streamlink was unable to process your query, because no plugin has been implemented for website {query}."
+        return f"Streamlink was unable to process your query, because no plugin has been implemented for website {query}"
     except PluginError as pex:
         return f"Streamlink couldn't process {query}, because of a Plugin error. Reason is as follows: {pex}"
