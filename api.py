67571991326b4d42b24b717a6530546a9e9d0620
LaneSh4d0w <lanesh4d0w@gmail.com>
2022-09-24 12:20:26 +0200

    final fix.

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 import streamlink
 from streamlink import NoPluginError, PluginError
-from streamlink.stream import DASHStream
+from streamlink.stream import DASHStream, HLSStream
 
 
 def get_streams(query):
@@ -13,16 +13,22 @@ def get_streams(query):
         if not streams:
             return "No streams found."
         for quality, link in streams:
-            # Suggest that if there's no manifest (live), give URL else use manifest URL.
-            return link.to_url() if ("live" not in quality or "best" in quality) \
-                                and "chunklist" in link.to_url() \
-                                or "live" in quality \
-                                and "best" not in quality \
-                                or type(DASHStream) \
-                                else link.to_manifest_url()
+            # Some DASH streams have got some interesting issues, hence we need to fix it directly.
+            # All HLS links should work with adaptive.
+            if type(link) is DASHStream:
+                return link.to_url()
+            elif type(link) is HLSStream:
+                return link.to_url() if "best" in quality and "chunklist" in link.to_url()\
+                                    or "live" in quality else link.to_manifest_url()
+    # Issue when getting data from query
     except ValueError as ex:
-        return f"Streamlink couldn't read {query}, for this reason : {ex}"
+        return f"Streamlink couldn't read {query}, {ex}"
+    # Trying to use Streamlink on an unsupported website
     except NoPluginError:
-        return f"Streamlink was unable to process your query, because no plugin has been implemented for website {query}"
+        return f"No plugin has been implemented for website {query}"
+    # Plugin issue when getting data from query
     except PluginError as pex:
-        return f"Streamlink couldn't process {query}, because of a Plugin error. Reason is as follows: {pex}"
+        return f"A Plugin error has occurred, {pex}"
+    # Anything else
+    except Exception as nex:
+        return f"Something went wrong somewhere, it seems, or else I wouldn't have '{nex}' as a result."
