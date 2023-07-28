2e5f41dc8550209e5c5b81e58a1159faf69445dd
LaneSh4d0w <lanesh4d0w@gmail.com>
2023-07-28 21:51:27 +0200

    Reimplement quality + code refactoring.

--- api.py
+++ api.py
@@ -4,47 +4,31 @@ from streamlink.stream import DASHStream
 import urllib.request
 
 
-def get_streams(query):
-    """
-    Get data streams
-    Returns: (links), Error string
-    """
+def get_streams(query, quality="best"):
     try:
         streams = streamlink.streams(query).items()
         if not streams:
             return "No streams found."
-        
-        for quality, link in streams:
-            if query.__contains__('dailymotion.com') or query.__contains__('dai.ly'):
+        for stream_quality, link in streams:
+            if quality is not None and quality != stream_quality:
+                continue
+            if 'dailymotion.com' in query or 'dai.ly' in query:
                 url = link.to_manifest_url()
-                if url.__contains__('https://www.dailymotion.com/cdn/live/video/'):
+                if 'https://www.dailymotion.com/cdn/live/video/' in url:
                     response = urllib.request.urlopen(url)
-                    data = response.read()
-                    text = data.decode('utf-8')
+                    data = response.read().decode('utf-8')
                     replacements = {'live-3': 'live-0', 'live-2': 'live-0', 'live-1': 'live-0'}
                     for key in replacements:
-                        if text.__contains__(key):
+                        if key in data:
                             l0_link = link.to_url()
                             l0_replace = l0_link.replace('live-0', replacements[key])
                             return l0_replace
                     return link.to_url()
                 else:
                     return url
-
-            if isinstance(link, DASHStream):
+            if isinstance(link, DASHStream) or ("best" in stream_quality and "chunklist" in link.to_url()) or "live" in stream_quality or "http" in stream_quality:
                 return link.to_url()
-            elif not isinstance(link, DASHStream):
-                if "best" in quality and "chunklist" in link.to_url() \
-                        or "live" in quality or "http" in quality:
-                    return link.to_url()
-                else:
-                    return link.to_manifest_url()
-
-    except ValueError as ex:
-        return f"Streamlink couldn't read {query}, {ex}"
-    except NoPluginError:
-        return f"No plugin has been implemented for website {query}"
-    except PluginError as pex:
-        return f"A Plugin error has occurred: {pex}"
-    except Exception as nex:
-        return f"Something went wrong: {nex}"
+        else:
+            return link.to_manifest_url()
+    except (ValueError, NoPluginError, PluginError, Exception) as ex:
+        return f"An error occurred: {ex}"
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
-from flask import Flask, request, redirect
+from flask import Flask, redirect, request
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from validators import url
 import validators
 from api import get_streams
 
@@ -18,54 +19,53 @@ limiter = Limiter(
 
 # Reads the URL parameters and redirects to Streamlink.
 def query_handler(args):
-    """Checks and tests arguments before serving the request"""
-    if not args.get("streaming-ip"):
+    streaming_ip = args.get("url")
+    provider = args.get("provider")
+    quality = args.get("quality")
+    if not streaming_ip:
         return "You didn't provide any URL."
-
-    # For Dacast, be warned we have MULTIPLE parameters. Get it if it exists.
-    if args.get("provider"):
-        valid = validators.url(args.get("streaming-ip"))
-        url = args.get("streaming-ip") + "&provider=" + args.get('provider')
-        return get_streams(url) if valid else "The URL you entered is not valid."
+    if not quality:
+        quality = "best"
+    if validators.url(streaming_ip):
+        if provider:
+            return get_streams(streaming_ip + "&provider=" + provider, quality)
+        else:
+            return get_streams(streaming_ip, quality)
     else:
-        valid = validators.url(args.get("streaming-ip"))
-        return get_streams(args.get("streaming-ip")) if valid else "The URL you entered is not valid."
+        return "The URL you entered is not valid."
 
 
 # Presentation page
 @app.route("/", methods=['GET'])
 def index():
-    """Displays the presentation page with instructions"""
-    return (
-        "This program allows you to access streams directly using Streamlink.\r\n"
-        "To process a link, append '/iptv-query?streaming-ip=*your URL*' to this webpage.\r\n"
-        "Please note that it only works with Streamlink-supported websites.\r\n"
-        "Enjoy! LaneSh4d0w. Special thanks to Keystroke for the API usage."
-    )
-
+    return """
+            This program allows you to access streams directly using Streamlink.
+            To process a link, append '/iptv-query?streaming-ip=*your URL*' to this webpage.
+            Please note that it only works with Streamlink-supported websites.
+            Enjoy! LaneSh4d0w. Special thanks to Keystroke for the API usage.
+           """
 
 # iptv-query route -> provides a link to Streamlink, analyzes the link
 # for correct plugin routing, and redirects (or displays) the stream link.
 @app.route("/iptv-query", methods=['GET'])
 @limiter.limit("20/minute")
-@limiter.limit("1/second")
 def home():
+    no_redirect = request.args.get("no_redirect")
     """Handles the IPTV query request and redirects to the stream link"""
-    response = query_handler(request.args)
-    valid2 = validators.url(response)
-    if response is None or not valid2:
+    try:
+        response = query_handler(request.args)
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
+    if not url(response):
         return response
-
-    return response if request.args.get("noredirect") == "yes" else redirect(response)
-
+    return redirect(response) if not no_redirect else response
 
 # Rate limiting system.
 @app.errorhandler(429)
 def ratelimit_handler(e):
     """Handles rate limit exceeded error"""
-    return f"{e}. To ensure fair access to the program, we are limiting the number of requests."
+    return "{}. To ensure fair access to the program, we are limiting the number of requests.".format(e)
 
 
-# Change the parameters according to your preferences: "host", "port", "threaded".
 if __name__ == '__main__':
     app.run(threaded=False, port=5000)
